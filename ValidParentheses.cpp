81575d54324bdcc2cd4249aee8be492aef3d39f7
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-10-11 10:35:10 +0200

    Initial code

---
+++ ValidParentheses.cpp
@@ -0,0 +1,9 @@
+#include "ValidParentheses.hpp"
+
+ValidParentheses::ValidParentheses() {
+    // Empty ctor
+}
+
+bool ValidParentheses::is_valid(const std::string& input) {
+    return true;
+}
---
+++ ValidParentheses.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class ValidParentheses {
+public:
+    ValidParentheses();
+    bool is_valid(const std::string& input);
+};
+
