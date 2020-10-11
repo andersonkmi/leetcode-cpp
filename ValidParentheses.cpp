b8788fb5d93e0bfccd01803a57aadbfd5e11dc35
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-10-11 11:05:56 +0200

    more code

--- ValidParentheses.cpp
+++ ValidParentheses.cpp
@@ -5,5 +5,14 @@ ValidParentheses::ValidParentheses() {
 }
 
 bool ValidParentheses::is_valid(const std::string& input) {
+    if (is_even_length(input)) {
+        return false;
+    }
+
     return true;
+}
+
+bool ValidParentheses::is_even_length(const std::string& input)
+{
+    return input.length() % 2 == 0;
 }
--- ValidParentheses.hpp
+++ ValidParentheses.hpp
@@ -9,5 +9,8 @@ public:
     ValidParentheses(const ValidParentheses&) = delete;
     ValidParentheses& operator= (const ValidParentheses&) = delete;
     bool is_valid(const std::string& input);
+    
+private:
+    bool is_even_length(const std::string&);
 };
 
