be860f780e0189d613517c97f6164ed3cb108c76
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-10-12 22:53:04 +0200

    initial code for exercise multiply string (wip)

---
+++ leetcode-library/MultiplyStrings.cpp
@@ -0,0 +1,12 @@
+#include "MultiplyStrings.hpp"
+
+MultiplyStrings::MultiplyStrings() 
+{
+    // empty constructor
+}
+
+std::string MultiplyStrings::multiply(const std::string& number1, const std::string& number2)
+{
+    std::string result("");
+    return result;
+}
---
+++ leetcode-library/MultiplyStrings.hpp
@@ -0,0 +1,24 @@
+#ifndef MULTIPLY_STRINGS_HPP__
+#define MULTIPLY_STRINGS_HPP__
+
+#ifdef LEETCODELIBRARY_EXPORTS
+#define LEETCODE_LIBRARY_API __declspec(dllexport)
+#else
+#define LEETCODE_LIBRARY_API __declspec(dllimport)
+#endif
+
+#include <string>
+
+using namespace std;
+
+class LEETCODE_LIBRARY_API MultiplyStrings
+{
+public:
+    MultiplyStrings();
+    MultiplyStrings(const MultiplyStrings&) = delete;
+    MultiplyStrings& operator=(const MultiplyStrings&) = delete;
+
+    std::string multiply(const std::string& number1, const std::string& number2);
+};
+
+#endif
