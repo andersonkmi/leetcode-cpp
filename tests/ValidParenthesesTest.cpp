e5f29c9548a72a13835619dc7fba71f4ce7e9a0a
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-01-30 23:04:03 +0100

    Add more tests

--- tests/ValidParenthesesTest.cpp
+++ tests/ValidParenthesesTest.cpp
@@ -2,9 +2,14 @@
 #include <catch2/catch.hpp>
 #include <ValidParentheses.hpp>
 
-TEST_CASE( "Quick check", "[main]" ) {
+TEST_CASE("Valid parentheses simple case") {
     ValidParentheses validParentheses;
     bool isValid = validParentheses.is_valid("()");
     REQUIRE(isValid == true);
 }
 
+TEST_CASE("Valid parentheses invalid simple case") {
+    ValidParentheses validParentheses;
+    bool isValid = validParentheses.is_valid("(");
+    REQUIRE(isValid == false);
+}
