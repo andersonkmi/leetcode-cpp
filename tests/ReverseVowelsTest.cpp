449e0064934de3430fc352d5b9208a63880a1333
= <=>
2024-02-04 18:06:43 +0100

    Add few more test cases for reverse vowels

--- tests/ReverseVowelsTest.cpp
+++ tests/ReverseVowelsTest.cpp
@@ -7,3 +7,17 @@ TEST_CASE("When reversing hello should return holle", "[reverse_vowels]") {
     std::string result = reverseVowels.reverseVowels("hello");
     CHECK(result == "holle");
 }
+
+
+TEST_CASE("When reversing egg should return egg", "[reverse_vowels]") {
+    ReverseVowels reverseVowels;
+    std::string result = reverseVowels.reverseVowels("egg");
+    CHECK(result == "egg");
+}
+
+
+TEST_CASE("When reversing omo should return egg", "[reverse_vowels]") {
+    ReverseVowels reverseVowels;
+    std::string result = reverseVowels.reverseVowels("omo");
+    CHECK(result == "omo");
+}
